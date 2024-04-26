37532aea0781890a8ad3654508c6ecacc3fa2126
Dylan Carroll <65430484+DylanScottCarroll@users.noreply.github.com>
2024-04-25 18:06:40 -0700

    Add update functionality to the Config class (#29)
    * Added updtate functionality to config class
    
    * Allowed for dicts or configs to be used in update

--- skeletonkey/config.py
+++ skeletonkey/config.py
@@ -39,7 +39,37 @@ class Config():
         else:
             self._init_from_dict(kwargs)
 
+    def update(self, update_config: dict|'Config'):
+        """
+        Take a config in some format and place those values into the config.
+        This will overwrite values if they are present or create them if they are not.
 
+        Args:
+            update_config (dict|Config): The keys/values to place into the config. If this is a dictionary,
+            it is expected that the keys are in dot notation.
+        """
+        if not isinstance(update_config, Config):
+            update_config = config_to_nested_config(Config(update_config))
+        self._update_from_config(update_config)
+
+
+    def _update_from_config(self, update_config: 'Config'):
+        """
+        Recursively place all of the values from update_config into self, overwriting them if they
+        exist and adding them if they do not.        
+        """
+
+        for k, v in update_config.__dict__.items():
+            if isinstance(v, Config):
+                if k not in self.__dict__.keys():
+                    self[k] = Config({})
+
+                self[k]._update_from_config(update_config[k])
+            
+            else:
+                self[k] = update_config[k]
+
+        
     def _init_from_dict(self, dictionary: dict):
         """
         Initialize the config from a dictionary
